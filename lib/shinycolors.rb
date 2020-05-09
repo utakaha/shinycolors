007a381a0449472b6dbac5d16a4e9f2ba3099829
iavivai <18yukitaka@gmail.com>
2020-05-10 01:34:59 +0900

    アイドルを扱うクラスを定義して、APIを作成

--- lib/shinycolors.rb
+++ lib/shinycolors.rb
@@ -1,4 +1,26 @@
-require "shinycolors/version"
+require 'shinycolors/version'
+require 'yaml'
 
 module ShinyColors
+  class Idol
+    class << self
+      def all
+        YAML.load_file('./lib/data/idol.yml').values.inject { |all, unit| all.merge(unit) }
+      end
+
+      def names
+        all.keys
+      end
+
+      def find(name)
+        idol = all[name.to_s]
+      end
+
+      Idol.names.each do |name|
+        define_method(name) do
+          Idol.find(name)
+        end
+      end
+    end
+  end
 end
