056ad5d0c5cace2b72c83875e409400a6ce1af4b
iavivai <18yukitaka@gmail.com>
2020-05-10 21:40:45 +0900

    アイドルを呼び出すとき、オブジェクトを作成するように変更
    個人の情報をオブジェクトとして管理した方が実装が楽なので、
    オブジェクトを作るように実装を変更した。それに伴って、
    initialize時にhashを渡す際、keyをシンボルにするために
    activesupportのhash拡張を追加した。

--- lib/shinycolors/idol.rb
+++ lib/shinycolors/idol.rb
@@ -1,14 +1,25 @@
 require 'yaml'
+require 'active_support/core_ext/hash'
 
 module ShinyColors
   class Idol
     class NotFoundError < StandardError; end
 
+    def initialize(name:, cv:, age:, birthplace:, birthday:)
+      @name = name
+      @cv = cv
+      @age = age
+      @birthplace = birthplace
+      @birthday = birthday
+    end
+
+    attr_reader :name, :cv, :age, :birthplace, :birthday
+
     class << self
       def all
         YAML.load_file('./data/idol.yml').each_with_object({}) do |(_, values), result|
           result.merge!(values['idols'])
-        end
+        end.deep_symbolize_keys!
       end
 
       def names
@@ -16,18 +27,18 @@ module ShinyColors
       end
 
       def find(name)
-        idol = all[name.to_s]
+        idol = Idol.new(**all[name])
         idol.nil? ? raise(IdolNotFoundError) : idol
       end
 
       def display(name)
         idol = find(name)
         puts <<~PRETTY
-          名前:   #{idol['name']}
-          cv:     #{idol['cv']}
-          年齢:   #{idol['age']}
-          出身地: #{idol['birthplace']}
-          誕生日: #{idol['birthday']}
+          名前:   #{idol.name}
+          cv:     #{idol.cv}
+          年齢:   #{idol.age}
+          出身地: #{idol.birthplace}
+          誕生日: #{idol.birthday}
         PRETTY
       end
     end
