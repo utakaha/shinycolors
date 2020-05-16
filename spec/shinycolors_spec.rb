fad787ed2241892f5970e81f89d26d38089ecabe
iavivai <18yukitaka@gmail.com>
2020-05-16 23:05:31 +0900

    Fix variable name.

--- spec/shinycolors_spec.rb
+++ spec/shinycolors_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe ShinyColors do
   describe '樋口円香' do
-    let(:madoka_attr) do
+    let(:madoka_attrs) do
       { name: '樋口 円香',
         cv: '土屋 李央',
         age: 17,
@@ -12,12 +12,12 @@ RSpec.describe ShinyColors do
     end
 
     it 'フルネームで呼び出せる' do
-      expect(described_class.madoka_higuchi).to have_attributes(madoka_attr)
+      expect(described_class.madoka_higuchi).to have_attributes(madoka_attrs)
     end
 
     it 'あだ名で呼び出せる' do
-      expect(described_class.madoka).to have_attributes(madoka_attr)
-      expect(described_class.higuchi).to have_attributes(madoka_attr)
+      expect(described_class.madoka).to have_attributes(madoka_attrs)
+      expect(described_class.higuchi).to have_attributes(madoka_attrs)
     end
   end
 end
